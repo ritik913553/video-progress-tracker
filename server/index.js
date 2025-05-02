4568e5d4a5ea8928ec8d56138114523d4ce7f632
ritik913553 <ritikgupta9135@gmail.com>
2025-05-02 15:14:55 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -4,26 +4,21 @@ import { app } from "./app.js";
 import path from "path";
 import express from "express";
 
-
 // ----------------code for deployment------------------
 if (process.env.NODE_ENV === "production") {
     const dirPath = path.resolve();
-    app.use(express.static("./client/dist"));
+    app.use(express.static(path.join(dirPath, "client/dist")));
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(dirPath, "./client/dist", "index.html"));
+        res.sendFile(path.resolve(dirPath, "client/dist", "index.html"));
     });
-  }
-
-
+}
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(` Server is running at port : ${process.env.PORT}`);
-        
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server is running at port : ${process.env.PORT}`);
+        });
     })
-})
-.catch((err)=>{
-    console.log("MONGO db conneection failed !!!",err); 
-    
-})
+    .catch((err) => {
+        console.log("MONGO db conneection failed !!!", err);
+    });
